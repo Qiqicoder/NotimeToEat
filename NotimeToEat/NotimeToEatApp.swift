ab2e9b2a4131fd1ca69a1a23ba420faab6553816
CharlesDaiii <rd10@illinois.edu>
2025-03-21 12:05:10 -0400

    Initial Commit

---
+++ NotimeToEat/NotimeToEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotimeToEatApp.swift
+//  NotimeToEat
+//
+//  Created by CharlesDai on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct NotimeToEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotimeToEatTests/NotimeToEatTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotimeToEatTests.swift
+//  NotimeToEatTests
+//
+//  Created by CharlesDai on 3/21/25.
+//
+
+import Testing
+@testable import NotimeToEat
+
+struct NotimeToEatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
